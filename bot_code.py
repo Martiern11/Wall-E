eb4e4e23655423031d490c3f64224ad1476b3e4b
Ernesto Martinez <147661396+Martiern11@users.noreply.github.com>
2024-06-10 10:56:51 -0700

    Create bot_code.py

---
+++ bot_code.py
@@ -0,0 +1,55 @@
+# writing the code bot #
+# importing modules #
+import discord 
+import os 
+import random 
+from dotenv import load_dotenv 
+from ec2_metadata import ec2_metadata 
+
+print(ec2_metadata.region) 
+print(ec2_metadata.instance_id)
+
+# Initializing variables #
+load_dotenv() 
+ 
+# Creating instancec for the discord bot #
+
+client = discord.Client(messages=True, guilds=True)
+token = str(os.getenv('TOKEN')) #Creating client to send a request to disord API #
+
+# Initializing the Bot #
+@client.event 
+async def on_ready(): 
+    print("Logged in as a bot {0.user}".format(client)) 
+
+# Array of jokes for the bot to tell #
+jokes_responses = {
+    "Why do programmers prefer dark mode?" : "Because light attracts bugs!",
+    "Why did the programmer quit his job?" : "Because he didn't get arrays"
+}
+
+# Setting appropiate response to user meassage #    
+@client.event 
+async def on_message(message): 
+    username = str(message.author).split("#")[0] 
+    channel = str(message.channel.name) 
+    user_message = str(message.content) 
+  
+    print(f'Message {user_message} by {username} on {channel}') 
+  
+    if message.author == client.user: 
+        return
+  
+    if channel == "general": 
+        if user_message.lower() == "Hello" or user_message.lower() == "hi": 
+            await message.channel.send(f'Hola {username}') 
+            return
+        elif user_message.lower() == "bye": 
+            await message.channel.send(f'Adios {username}') 
+        elif user_message.lower() == "tell me a joke": 
+            joke = random.choice(list(jokes_responses.keys()))
+            response = jokes_responses[joke]
+            await message.channel.send(f'{joke}\n\n{response}')
+
+# Argument to run the bot by calling an event #         
+client.run(token)  
